11d7b9b4dc3c2d29b93ce8cc1642c0be518b5ab0
Sasha5832 <115210545+Sasha5832@users.noreply.github.com>
2025-04-04 10:10:03 +0200

    Update ShoppingCart.py

--- 169405/src/ShoppingCart.py
+++ 169405/src/ShoppingCart.py
@@ -1,13 +1,14 @@
 class ShoppingCart:
+
     def __init__(self, items):
-       if not isinstance(items, list):
-           raise TypeError
+        if not isinstance(items, list):
+            raise TypeError
 
-       for elem in items:
-           if not isinstance(elem, str):
-               raise ValueError
+        for elem in items:
+            if not isinstance(elem, str):
+                raise ValueError
 
-       self.items = items.copy()
+        self.items = items
 
     def __str__(self):
         return f"Shopping card: {self.items}."
@@ -15,16 +16,21 @@ class ShoppingCart:
     def add_item(self, value):
         if not isinstance(value, str):
             raise ValueError
+
         self.items.append(value)
 
-    def remove_intem(self, index):
+    def remove_item(self, index):
         if not isinstance(index, int):
             raise TypeError
 
-        if 0> index or index >= len(self.items):
+        if 0 > index or index >= len(self.items):
             raise IndexError
 
         self.items.remove(self.items[index])
+
+
+    def __eq__(self, other):
+        return self.items == other.items
 
 
 if __name__ == "__main__":
@@ -33,5 +39,5 @@ if __name__ == "__main__":
     s1.add_item("Apple")
     s1.add_item("Milk")
     print(s1)
-    s1.remove_intem(0)
+    s1.remove_item(0)
     print(s1)
